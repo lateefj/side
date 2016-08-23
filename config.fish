506e88a6b565f5bd9a8fc945ff54f69be5f567e5
Lateef Jackson <lateef.jackson@gmail.com>
2016-08-22 22:14:42 -0700

    Prepare to add nix to the mix

--- config.fish
+++ config.fish
@@ -85,3 +85,8 @@ end
 if test -e $HOME/.side/config.fish
   source $HOME/.side/config.fish
 end
+
+# NIX
+if test -e $HOME/.nix-profile/etc/profile.d/nix.sh
+#eval (bash -c "source ~/.nix-profile/etc/profile.d/nix.sh; fish --command 'echo set -x NIX_PATH \"\$NIX_PATH\"\;; echo set -x PATH \"\$PATH\"\;; echo set -x SSL_CERT_FILE \"\$SSL_CERT_FILE\"'")
+end
