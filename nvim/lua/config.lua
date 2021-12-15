b49726f67a1cde9d95d9d4d4bfe948f2de97e4b8
Lateef Jackson <lateef@troymedicare.com>
2021-12-15 09:20:55 -0800

    Remove old LSP syntax

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -113,23 +113,6 @@ require'lspconfig'.sqls.setup{
     require'sqls'.setup{}
   end
 }
--- Language server
-local function setup_servers()
-  require'lspinstall'.setup()
-  local servers = require'lspinstall'.installed_servers()
-  for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{}
-  end
-end
-
-setup_servers()
-
--- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-require'lspinstall'.post_install_hook = function ()
-  setup_servers() -- reload installed servers
-  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-end
-
 -- Flutter configuration
 require("flutter-tools").setup{} -- use defaults
 require("telescope").load_extension("flutter")
