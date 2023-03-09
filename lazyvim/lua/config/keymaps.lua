4f43070973ad66de2c138accd4e3dc8f1f40821c
Lateef Jackson <lateef.jackson@gmail.com>
2023-03-09 09:49:12 -0800

    Add lazyvim option

---
+++ lazyvim/lua/config/keymaps.lua
@@ -0,0 +1,26 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+map("n", "<leader>h", "<C-W>h", { desc = "left window" })
+map("n", "<leader>j", "<C-W>j", { desc = "right window" })
+map("n", "<leader>k", "<C-W>k", { desc = "up window" })
+map("n", "<leader>l", "<C-W>l", { desc = "down window" })
+map("n", "<leader>x", "<C-W>c", { desc = "close window" })
+map("n", "<leader>s", "<C-W>s", { desc = "horizontal split" })
+map("n", "<leader>v", "<C-W>v", { desc = "vertical split" })
+map("n", "<leader>e", "<C-W>=", { desc = "equalize window splits" })
+map("n", ">", "<C-W>>", { desc = "Increase window width" })
+map("n", "<", "<C-W><", { desc = "Descrease window width" })
+map("n", "-", ":resize -1<CR>", { desc = "Decrease window height" })
+map("n", "+", ":resize +1<CR>", { desc = "Increase window height" })
---
+++ lazyvim/lua/plugins/lhj.lua
@@ -0,0 +1,20 @@
+return {
+  -- Go
+  {
+    "fatih/vim-go",
+  },
+  -- Fish shell
+  { "dag/vim-fish" },
+  -- Awk
+  { "vim-scripts/awk.vim" },
+  -- clang
+  { "justmao945/vim-clang" },
+  -- Prolog
+  { "soli/prolog-vim" },
+  -- Zig
+  { "ziglang/zig.vim" },
+  -- Markdown
+  { "ellisonleao/glow.nvim" },
+  -- asciidoctor
+  { "habamax/vim-asciidoctor" },
+}
