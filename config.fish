926a3895d9bd86855bc11f46652205bc89fcb0cf
Lateef Jackson <lateef.jackson@gmail.com>
2023-07-14 07:36:47 -0700

    Add minikube alias

--- config.fish
+++ config.fish
@@ -14,44 +14,44 @@ set -x EDITOR nvim
 fish_vi_key_bindings
 
 switch $PLATFORM
-  case Linux
-    source $SIDE_HOME/linux.fish
-  case Darwin
-    source $SIDE_HOME/darwin.fish
-  case FreeBSD
-    source $SIDE_HOME/bsd.fish
+    case Linux
+        source $SIDE_HOME/linux.fish
+    case Darwin
+        source $SIDE_HOME/darwin.fish
+    case FreeBSD
+        source $SIDE_HOME/bsd.fish
 end
 
 # Install a $HOME/local directory for installing local package
 if test -e $HOME/local/bin
-  set -xg PATH $PATH $HOME/local/bin
+    set -xg PATH $PATH $HOME/local/bin
 end
 # Path for binaries installed in home
 if test -e $HOME/bin
-  set -xg PATH $PATH $HOME/bin 
+    set -xg PATH $PATH $HOME/bin
 end
 
 # tmux unicode
 function utmux
-  tmux -u $argv
+    tmux -u $argv
 end
 
 # Set environment variables
 if test -e $HOME/bin
-  set -xg PATH $PATH $HOME/bin
+    set -xg PATH $PATH $HOME/bin
 end
 if test -e $HOME/go/bin
-  set -xg PATH $HOME/go/bin $PATH 
+    set -xg PATH $HOME/go/bin $PATH
 end
 if test -e $HOME/local/go/bin
-  set -xg PATH $HOME/local/go/bin $PATH
-  set -xg GOBIN $HOME/local/bin
+    set -xg PATH $HOME/local/go/bin $PATH
+    set -xg GOBIN $HOME/local/bin
 end
 if test -e $HOME/local/ziglang
-  set -xg PATH $PATH $HOME/local/ziglang
+    set -xg PATH $PATH $HOME/local/ziglang
 end
 if test -e $HOME/local/zls
-  set -xg PATH $PATH $HOME/local/zls
+    set -xg PATH $PATH $HOME/local/zls
 end
 
 # Fortran
@@ -62,93 +62,104 @@ set -xg CFLAGS -O2
 
 # Mercurial alias 
 function hs
-  hg status
+    hg status
 end
 
 # Git alias
 function gs
-  git status
+    git status
 end
 
 # Local nim
 if test -e $HOME/local/nim
-  set -xg PATH $PATH $HOME/local/nim/bin
+    set -xg PATH $PATH $HOME/local/nim/bin
 end
 
 # Dart library installed
 if test -e /usr/lib/dart/bin/
-  set -xg PATH $PATH /usr/lib/dart/bin/
+    set -xg PATH $PATH /usr/lib/dart/bin/
+end
+
+# Minikube 
+set minikube_path (which minikube)
+if [ $minikube_path ]
+    test -x $minikube_path
+    function kubectl
+        minikube kubectl -- $argv
+    end
 end
 
 
 if test -e $HOME/.pyenv/bin/pyenv
-  set -xg PATH $HOME/.pyenv/bin/ $PATH
+    set -xg PATH $HOME/.pyenv/bin/ $PATH
 end
 
 function initpenv
-	# If pyenv exists then run it
-	set pyenv_exists (which pyenv)
-	if [ $pyenv_exists ];  test -x $pyenv_exists
-		set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-		status is-login; and pyenv init --path | source
-		pyenv init - | source
+    # If pyenv exists then run it
+    set pyenv_exists (which pyenv)
+    if [ $pyenv_exists ]
+        test -x $pyenv_exists
+        set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+        status is-login; and pyenv init --path | source
+        pyenv init - | source
 
-		# Found this saves a lot of startup time (like 40%) by not calling it based on using this command
-		# echo '' > startup.txt && nvim --startuptime startup.txt
-		if test -z "$VIMRUNTIME"
-			pyenv rehash
-		end
-	end
+        # Found this saves a lot of startup time (like 40%) by not calling it based on using this command
+        # echo '' > startup.txt && nvim --startuptime startup.txt
+        if test -z "$VIMRUNTIME"
+            pyenv rehash
+        end
+    end
 end
 
 # Use neovim if it exists
 function which_editor
-  set nvim_path (which nvim)
-  if [ $nvim_path ];  test -x $nvim_path
-    echo nvim
-  else
-    echo vim
-  end
+    set nvim_path (which nvim)
+    if [ $nvim_path ]
+        test -x $nvim_path
+        echo nvim
+    else
+        echo vim
+    end
 end
 
 # TODO: Figure this out
-function e 
-  set cmd (which_editor)
-  set TERM screen-256color
-  eval $cmd $argv
+function e
+    set cmd (which_editor)
+    set TERM screen-256color
+    eval $cmd $argv
 end
 
 
 # Gui vim
 function ge
-  switch $PLATFORM
-    case Linux
-      gvim $argv
-    case Darwin
-      mvim $argv
-  end
+    switch $PLATFORM
+        case Linux
+            gvim $argv
+        case Darwin
+            mvim $argv
+    end
 end
 
 function ll
-  ls -lh $argv
+    ls -lh $argv
 end
 
 
 function mkenv
-  mkdir -p $HOME/.venv
-  pyvenv $HOME/.venv/$argv
+    mkdir -p $HOME/.venv
+    pyvenv $HOME/.venv/$argv
 end
 
 function wrkenv
-  source $HOME/.venv/$argv/bin/activate.fish
+    source $HOME/.venv/$argv/bin/activate.fish
 end
 
 
 if test -e $SIDE_HOME/prompt.fish
-  source $SIDE_HOME/prompt.fish
+    source $SIDE_HOME/prompt.fish
 end
 if test -e $HOME/.side/config.fish
-  source $HOME/.side/config.fish
+    source $HOME/.side/config.fish
 end
 
 
@@ -156,29 +167,32 @@ end
 set -xg C_INCLUDE_PATH . $C_INCLUDE_PATH
 # Local zig installation
 if test -e $HOME/local/zig/zig
-  # Add zig to path
-  set -xg PATH $PATH $HOME/local/zig
+    # Add zig to path
+    set -xg PATH $PATH $HOME/local/zig
 end
 
 
 # Set chrome executable if chromium
 if test -e /usr/bin/chromium
-  set -xg CHROME_EXECUTABLE /usr/bin/chromium
+    set -xg CHROME_EXECUTABLE /usr/bin/chromium
 end
 
 
 function random_string --description "random_string 32"
-  switch $PLATFORM
-  case Darwin
-    head -c $argv[1] < /dev/random | base64
-  case '*'
-    head /dev/urandom | tr -dc A-Za-z0-9 | head -c $argv[1] ; echo ''
-  end
+    switch $PLATFORM
+        case Darwin
+            head -c $argv[1] </dev/random | base64
+        case '*'
+            head /dev/urandom | tr -dc A-Za-z0-9 | head -c $argv[1]
+            echo ''
+    end
 end
 
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/lateef/local/google-cloud-sdk/path.fish.inc' ]; . '/Users/lateef/local/google-cloud-sdk/path.fish.inc'; end
+if [ -f '/Users/lateef/local/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/lateef/local/google-cloud-sdk/path.fish.inc'
+end
 
 # FZF search
 set -xg FZF_DEFAULT_COMMAND "rg --files --hidden --follow --glob '!.git'"
