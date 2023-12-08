f494822406980efb7733f3dd8366c4f0f64cd5c0
Lateef Jackson <lateef.jackson@gmail.com>
2023-12-07 22:50:29 -0800

    Fix BSD library path

--- bsd.fish
+++ bsd.fish
@@ -1,6 +1,8 @@
 set -xg LC_ALL C
 set -xg LANG en_US.UTF-8
 
+set -xg LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/local/lib
+set -xg LIBRARY_PATH $LD_LIBRARY_PATH
 function mxv
   mixer vol $argv
 end
@@ -12,4 +14,5 @@ end
 if test -e $HOME/local/go/bin
   set -xg PATH $PATH $HOME/local/go/bin
 end
+
 
