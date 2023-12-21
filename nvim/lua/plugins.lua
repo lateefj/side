3de664eb975ea0ec845fa39ed67f2096293429fe
Lateef Jackson <lateef.jackson@gmail.com>
2023-12-20 21:53:45 -0800

    Night color

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -215,6 +215,14 @@ require("lazy").setup({
 })
 
 -- Plugin Config:
+require("tokyonight").setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = false, -- Enable this to disable setting the background color
+})
+
 require("lualine").setup({
   options = {
     theme = "tokyonight",
