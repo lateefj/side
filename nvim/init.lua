ccbf38774c1c2068e7f987e7450623afc1d38768
Lateef Jackson <lateef.jackson@gmail.com>
2024-08-07 23:40:01 -0700

    Move to mini.nvim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,20 +6,37 @@ vim.opt.mouse = "a"
 -- "Bring me to your Leader!!!"
 vim.g.mapleader = ","
 
+-- Mini.nvim
+-- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/echasnovski/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+-- Set up 'mini.deps' (customize to your liking)
+require('mini.deps').setup({ path = { package = path_package } })
 -- Install Lazy
 --
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not vim.loop.fs_stat(lazypath) then
+--   vim.fn.system({
+--     "git",
+--     "clone",
+--     "--filter=blob:none",
+--     "https://github.com/folke/lazy.nvim.git",
+--     "--branch=stable", -- latest stable release
+--     lazypath,
+--   })
+-- end
+-- vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
 require("plugins")
--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -58,6 +58,64 @@ require('mini.trailspace').setup()
 require('mini.visits').setup()
 
 
+-- Possible to immediately execute code which depends on the added plugin
+require('nvim-treesitter.configs').setup({
+  ensure_installed = { 'lua', 'vimdoc' },
+  highlight = { enable = true },
+})
+require("go").setup()
+
+-- Lsp Format
+require("lsp-format").setup {}
+
+local cmp = require("cmp")
+cmp.setup({
+  mapping = {
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        local entry = cmp.get_selected_entry()
+        if not entry then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          cmp.confirm()
+        end
+      else
+        fallback()
+      end
+    end, { "i", "s", "c" }),
+  },
+})
+-- Lsp Zero
+local lsp_zero = require('lsp-zero')
+
+-- lsp_attach is where you enable features that only work
+-- if there is a language server active in the file
+local lsp_attach = function(client, bufnr)
+  local opts = { buffer = bufnr }
+
+  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+end
+
+lsp_zero.extend_lspconfig({
+  sign_text = true,
+  lsp_attach = lsp_attach,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
+
+
+require('lspconfig').lua_ls.setup({})
+require('lspconfig').gopls.setup({})
+require('lspconfig').zls.setup({})
+require('lspconfig').pylsp.setup({})
 -- Run gofmt + goimports on save
 
 local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
@@ -69,17 +127,17 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   group = format_sync_grp,
 })
 
-require('asciidoc-preview').setup(
-  {
-    server = {
-      converter = 'cmd',
-      port = 11235,
-    },
-    preview = {
-      position = 'current',
-    },
-  }
-)
+-- require('asciidoc-preview').setup(
+--   {
+--     server = {
+--       converter = 'cmd',
+--       port = 11235,
+--     },
+--     preview = {
+--       position = 'current',
+--     },
+--   }
+-- )
 local actions = require("telescope.actions")
 local open_with_trouble = require("trouble.sources.telescope").open
 
